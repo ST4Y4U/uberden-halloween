fb898db1210ff958cf899d3380e33db3feeccb4b
ST4Y4U <sarang120807@gmail.com>
2025-10-30 15:48:58 +0900

    Update Boot.ts

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,22 +1,7 @@
 import Phaser from "phaser";
 
 export default class Boot extends Phaser.Scene {
-  constructor() {
-    super("Boot");
-  }
-
-  preload() {
-    // (나중에 자산 로드 추가)
-  }
-
-  create() {
-    this.add.text(640, 360, "Boot Scene", {
-      fontFamily: "sans-serif",
-      fontSize: "28px",
-      color: "#ffffff"
-    }).setOrigin(0.5);
-
-    // ★ 바로 메인메뉴로 전환 (딜레이 없어도 됨)
-    this.scene.start("MainMenu");
-  }
+  constructor() { super("Boot"); }
+  preload() { /* 전역 폰트/공통 리소스 필요하면 여기서 */ }
+  create() { this.scene.start("MainMenu"); }
 }
